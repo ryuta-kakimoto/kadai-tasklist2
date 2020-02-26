c03da4def4f5312bcfd38c14cc6bb5ef2b8ffa00
ryuta.kakimoto <06gwfz98@gmail.com>
2020-02-26 11:40:25 +0000

    ユーザー登録

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,68 @@
+class TasksController < ApplicationController
+    before_action :require_user_logged_in, only: [:index, :show, :edit]
+  
+    
+  def index
+    if logged_in?
+    @tasks = current_user.tasks.build
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+ 
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+
+
+
+  private
+
+
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+end
